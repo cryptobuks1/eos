a9f3e98ab422a1fe31f11c9d14f8486097be3ae9
Matt Witherspoon <32485495+spoonincode@users.noreply.github.com>
2020-08-25 22:38:37 -0400

    TPM signature provider

---
+++ libraries/tpm-helpers/include/eosio/tpm-helpers/tpm-helpers.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <fc/crypto/public_key.hpp>
+#include <fc/crypto/signature.hpp>
+#include <fc/crypto/sha256.hpp>
+
+#include <boost/container/flat_set.hpp>
+
+#include <set>
+
+namespace eosio::tpm {
+
+class tpm_key {
+public:
+   tpm_key(const std::string& tcti, const fc::crypto::public_key& pubkey, const std::vector<unsigned>& pcrs);
+   ~tpm_key();
+
+   fc::crypto::signature sign(const fc::sha256& digest);
+
+private:
+   struct impl;
+   constexpr static size_t fwd_size = 256;
+   fc::fwd<impl,fwd_size> my;
+};
+
+boost::container::flat_set<fc::crypto::public_key> get_all_persistent_keys(const std::string& tcti);
+fc::crypto::public_key create_key(const std::string& tcti, const std::vector<unsigned>& pcrs);
+
+class swtpm {
+public:
+   swtpm();
+   ~swtpm();
+
+   std::string tcti() const;
+
+private:
+   struct impl;
+   constexpr static size_t fwd_size = 128;
+   fc::fwd<impl,fwd_size> my;
+};
+
+}
